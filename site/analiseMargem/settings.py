8b12b1a432182ae1ed0f1cb00c62fd4270acaeb7
Gabriel Brito <gbrito341@gmail.com>
2026-01-22 13:33:44 -0300

    Migrar para a VPS Testes para usar o banco sem interferir na aplicação atual

--- site/analiseMargem/settings.py
+++ site/analiseMargem/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt_BR'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
