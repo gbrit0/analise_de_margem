94c3b4587e72dcb3e626062bbc191b52bd1763e8
Gabriel Brito <gbrito341@gmail.com>
2026-04-02 14:17:59 -0300

    Settings para produção; static para servir o site

--- site/setup/settings.py
+++ site/setup/settings.py
@@ -33,11 +33,13 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'brg.datasetsolucoes.com.br',
     '172.49.49.6',
+    'margem.brggeradores.com.br'
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'brg.datasetsolucoes.com.br:7040',
+    'brg.datasetsolucoes.com.br:30035',
     '172.49.49.6:8000',
+    'margem.brggeradores.com.br'
 ]
 # Application definition
 
@@ -142,9 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
